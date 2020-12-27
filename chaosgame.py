a80ca9cffeb523bed0125c82c7dd3d7e3a132dc8
Gor Kettsyan <gket96@gmail.com>
2020-12-27 16:59:05 +0400

    pep8 style fixes

--- chaosgame.py
+++ chaosgame.py
@@ -1,31 +1,27 @@
 from random import randint
 import matplotlib.pyplot as plt
 
+
 def sierpinski(n):
     x = [2, 6, 4, 3]
     y = [3, 3, 14, 8]
-    
+
     for i in range(n):
         rand = randint(1, 3)
-        if rand == 1 :
-            x.append((x[0] + x[len(x) -1])/2)
+        if rand == 1:
+            x.append((x[0] + x[len(x) - 1])/2)
             y.append((y[0] + y[len(y) - 1])/2)
-                
 
         elif rand == 2:
-            x.append((x[1] + x[len(x) -1])/2)
+            x.append((x[1] + x[len(x) - 1])/2)
             y.append((y[1] + y[len(y) - 1])/2)
 
-        
         elif rand == 3:
-            x.append((x[2] + x[len(x) -1])/2)
+            x.append((x[2] + x[len(x) - 1])/2)
             y.append((y[2] + y[len(y) - 1])/2)
-
 
     plt.plot(x, y, 'b.')
     plt.show()
 
+
 sierpinski(100000)
-
-
-
--- chaosgame_anim.py
+++ chaosgame_anim.py
@@ -10,27 +10,22 @@ def siperpinski_triangle():
     def animate(i):
         rand = randint(1, 6)
         if rand == 1 or rand == 2:
-            x.append((x[0] + x[len(x) -1])/2)
+            x.append((x[0] + x[len(x) - 1])/2)
             y.append((y[0] + y[len(y) - 1])/2)
-                
 
         elif rand == 3 or rand == 4:
-            x.append((x[1] + x[len(x) -1])/2)
+            x.append((x[1] + x[len(x) - 1])/2)
             y.append((y[1] + y[len(y) - 1])/2)
 
-        
         elif rand == 5 or rand == 6:
-            x.append((x[2] + x[len(x) -1])/2)
+            x.append((x[2] + x[len(x) - 1])/2)
             y.append((y[2] + y[len(y) - 1])/2)
-        
 
-        ax1.scatter(x, y, color = 'g', s = 2)
-
+        ax1.scatter(x, y, color='g', s=2)
 
     fig = plt.figure()
-    ax1 = fig.add_subplot(1,1,1)
-    ani = animation.FuncAnimation(fig, animate, interval = 100, frames = 1)
-
+    ax1 = fig.add_subplot(1, 1, 1)
+    animation.FuncAnimation(fig, animate, interval=100, frames=1)
 
     plt.show()
 
